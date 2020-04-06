6114602e18b20561d6a6ce87c3a754a552e4e66b
gonvlad <gon.vlad02@gmail.com>
2020-04-06 10:35:02 +0300

    fixed code style mistakes in Runner.java

--- practice2/src/com/company/practice2/Runner.java
+++ practice2/src/com/company/practice2/Runner.java
@@ -4,9 +4,9 @@ import by.gsu.pms.*;
 
 public class Runner {
     public static void main(String[] args) {
-
         UniformMaterial wood = new UniformMaterial("oak", 2345);
         UniformMaterial steel = new UniformMaterial("steel", 7890);
+        
         UniformMaterialSubject chair = new UniformMaterialSubject("chair", wood, 0.04);
         System.out.println(chair);
 
